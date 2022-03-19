f88c9b43c73035c731a3cddf573c6109990c958a
Prescott Murphy <prurph@gmail.com>
2022-03-19 12:41:32 -0400

    Create hello library and command

---
+++ hello/hello_test.go
@@ -0,0 +1,19 @@
+package hello_test
+
+import (
+	"bytes"
+	"hello"
+	"testing"
+)
+
+func TestPrintsHelloMessageToTerminal(t *testing.T) {
+	t.Parallel()
+	fakeTerminal := &bytes.Buffer{}
+	p := &hello.Printer{Output: fakeTerminal}
+	p.Print()
+	want := "Hello, world\n"
+	got := fakeTerminal.String()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
