fe9545bd1ebbaf98e53cb1957986cb81f48edd3d
Prescott Murphy <prurph@gmail.com>
2022-04-01 22:25:45 -0400

    Prototype battery ToJSON functionality

---
+++ battery-json/battery.go
@@ -0,0 +1,19 @@
+package battery
+
+import "encoding/json"
+
+type Battery struct {
+	Name             string
+	ID               int
+	ChargePercent    int
+	TimeToFullCharge string
+	Present          bool
+}
+
+func (b Battery) ToJSON() string {
+	output, err := json.MarshalIndent(b, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	return string(output)
+}
